74478ee81a1b93a633d341ed943848aa464cbca7
Ryan Sjostrand <ryansjostrand@gmail.com>
2021-11-04 20:27:36 -0500

    Add Arm subsystem to drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpMode.java
@@ -1,18 +1,10 @@
 package org.firstinspires.ftc.teamcode;
-import com.arcrobotics.ftclib.command.CommandOpMode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.robocol.Command;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.robocol.Command;
 
 @TeleOp
 
@@ -24,21 +16,23 @@ public class DriveOpMode extends CommandOpMode {
     private Outtake outtake;
     private IntakeRamp intakeramp;
     private IntakeSpinner intakeSpinner;
+    private Arm arm;
 
     @Override
     public void initialize(){
         drive = new Drive(hardwareMap);
         duckSpinner = new DuckSpinner(hardwareMap);
         outtake = new Outtake(hardwareMap);
-        
+        arm = new Arm(hardwareMap, telemetry);
+
         drive.setDefaultCommand
                 (new DriveWithGamepadCommand(gamepad1, drive)
         );
         
         GamepadEx driver = new GamepadEx(gamepad1);
 
-        GamepadButton moveUpButton = new GamepadButton(driver, GamepadKeys.Button.A);
-        moveUpButton.whileHeld(new MoveDuckSpinnerSpinCommand(duckSpinner));
+        GamepadButton duckSpinnerButton = new GamepadButton(driver, GamepadKeys.Button.A);
+        duckSpinnerButton.whileHeld(new MoveDuckSpinnerSpinCommand(duckSpinner));
         
         GamepadButton moveUpButton = new GamepadButton(driver, GamepadKeys.Button.A);
         moveUpButton.whileHeld(new MoveOuttakeUpCommand(outtake));
@@ -48,16 +42,22 @@ public class DriveOpMode extends CommandOpMode {
         intakeramp = new IntakeRamp(hardwareMap);
         intakeSpinner = new IntakeSpinner(hardwareMap);
 
-        GamepadButton moveUpButton = new GamepadButton(driver, GamepadKeys.Button.A);
-        moveUpButton.whileHeld(new MoveIntakeRampUpCommand(intakeramp));
+        GamepadButton intakeRampUp = new GamepadButton(driver, GamepadKeys.Button.A);
+        intakeRampUp.whileHeld(new MoveIntakeRampUpCommand(intakeramp));
 
-        GamepadButton moveDownButton = new GamepadButton(driver, GamepadKeys.Button.B);
-        moveDownButton.whileHeld(new MoveIntakeRampDownCommand(intakeramp));
+        GamepadButton intakeRampDown = new GamepadButton(driver, GamepadKeys.Button.B);
+        intakeRampDown.whileHeld(new MoveIntakeRampDownCommand(intakeramp));
 
         GamepadButton spinIntakeIn = new GamepadButton(driver, GamepadKeys.Button.X);
 
         spinIntakeIn.whileHeld(new SpinIntakeCommand(intakeSpinner));
         GamepadButton spinIntakeOut = new GamepadButton(driver, GamepadKeys.Button.Y);
         spinIntakeOut.whileHeld(new SpinOuttakeCommand(intakeSpinner));
+
+        GamepadButton armHomeButton = new GamepadButton(driver, GamepadKeys.Button.A);
+        armHomeButton.whenPressed(new HomeArmCommand(arm));
+
+        GamepadButton armCommandButton = new GamepadButton(driver, GamepadKeys.Button.Y);
+        armCommandButton.whenPressed(new ControlArmCommand(arm,1000, telemetry));
 }
 }
