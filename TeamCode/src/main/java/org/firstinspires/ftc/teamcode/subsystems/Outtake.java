2b908b0d3af21f3500534a08a30a503a2898c5ad
rkqiusn <92832957+rkqiusn@users.noreply.github.com>
2022-01-04 19:59:13 -0600

    Update Outtake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
@@ -6,25 +6,25 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Outtake extends SubsystemBase {
 
-    private Servo servo; 
+    private Servo servo;
+    private double offset = 0.05 ;
 
     public Outtake(HardwareMap hardwareMap) {
         servo = hardwareMap.get(Servo.class, "servoTheGreat");
     }
 
     public void in() {
-        servo.setPosition(0.675);
+        servo.setPosition(1.0-offset);
     }
 
 
     public void out() {
-        servo.setPosition(0.1);
+        servo.setPosition(0.425-offset);
     }
 
     public void stow() {
-        servo.setPosition(0.4);
+        servo.setPosition(0.725-offset);
     }
-
 
 
 
