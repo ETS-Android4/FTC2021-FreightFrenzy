d85693f1c6afcd3437022dd18de0bc57fab87f20
Ryan Sjostrand <ryansjostrand@gmail.com>
2022-01-06 15:43:00 -0600

    Aruco Pipeline example

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionPipeline.java
@@ -0,0 +1,133 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import org.opencv.aruco.DetectorParameters;
+import org.opencv.aruco.Dictionary;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.aruco.Aruco;
+
+public class VisionPipeline extends OpenCvPipeline {
+
+    public enum MarkerPlacement {
+        LEFT,
+        CENTER,
+        RIGHT,
+        UNKNOWN
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+//        System.out.println(input.dump());
+        // I/System.out: Mat [ 240*320*CV_8UC4, isCont=true, isSubmat=false, nativeObj=0xe870b880, dataAddr=0xce783000 ]
+        System.out.println(input.toString());
+
+        Imgproc.cvtColor(input, input, Imgproc.COLOR_RGB2BGR);
+
+        List<Mat> markerCorners = new ArrayList<>();
+        MatOfInt markerIDs = new MatOfInt();
+
+        //
+        // Detect Aruco markers
+        //
+        Dictionary dict = Aruco.getPredefinedDictionary(Aruco.DICT_6X6_250);
+        DetectorParameters parameters = DetectorParameters.create();
+        Aruco.detectMarkers(input, dict, markerCorners, markerIDs, parameters);
+
+        //
+        // Determine which team marker spot it is on (If markers are present)
+        //
+        Rect leftArea = new Rect(
+                new Point(0,0),
+                new Point(input.width()*(1.0/3.0), input.height())
+        );
+        Rect centerArea = new Rect(
+                new Point(input.width()*(1.0/3.0),0),
+                new Point(input.width()*(2.0/3.0), input.height())
+        );
+        Rect rightArea = new Rect(
+                new Point(input.width()*(2.0/3.0),0),
+                new Point(input.width(), input.height())
+        );
+
+        MarkerPlacement markerPlacement = MarkerPlacement.UNKNOWN;
+
+        int[] markerIDArray = new int[0];
+        if (!markerCorners.isEmpty()) {
+            markerIDArray = markerIDs.toArray();
+        }
+        for (int i = 0; i < markerCorners.size(); i++) {
+            int markerID = markerIDArray[i];
+            Mat corners = markerCorners.get(i);
+            Moments moments = Imgproc.moments(corners);
+
+            // https://docs.opencv.org/3.4/dd/d49/tutorial_py_contour_features.html
+            double cx = moments.m10/moments.m00;
+            double cy = moments.m01/moments.m00;
+            Point centerOfMass = new Point(cx, cy);
+
+            // We only care about marker with id 23
+            if (markerID != 23) {
+                System.out.println("Ignoring marker: id="+markerID + " cx="+cx + " cy="+cy);
+                continue;
+            }
+
+            // Figure out which region of the image the marker is in.
+            if(leftArea.contains(centerOfMass)) {
+                markerPlacement = MarkerPlacement.LEFT;
+            } else if(centerArea.contains(centerOfMass)) {
+                markerPlacement = MarkerPlacement.CENTER;
+            } else if(rightArea.contains(centerOfMass)) {
+                markerPlacement = MarkerPlacement.RIGHT;
+            }
+
+            System.out.println("Found marker: id="+markerID + " placement=" + markerPlacement+ " cx="+cx + " cy="+cy);
+            break;
+        }
+
+
+        //
+        // Annotate the source image
+        //
+        Mat output = input.clone();
+
+        double leftAlpha = 0.1;
+        double centerAlpha = 0.1;
+        double rightAlpha = 0.1;
+
+        if (markerPlacement == MarkerPlacement.LEFT) {
+            leftAlpha = 0.75;
+        } else if (markerPlacement == MarkerPlacement.CENTER) {
+            centerAlpha = 0.75;
+        } else if (markerPlacement == MarkerPlacement.RIGHT) {
+            rightAlpha = 0.75;
+        }
+
+        drawTransparentRect(leftArea ,new Scalar(0,0,255), leftAlpha, output);
+        drawTransparentRect(centerArea ,new Scalar(0,255,0), centerAlpha, output);
+        drawTransparentRect(rightArea ,new Scalar(255,0,0),  rightAlpha, output);
+
+        Aruco.drawDetectedMarkers(output, markerCorners, markerIDs);
+
+
+        return output;
+    }
+
+    private void drawTransparentRect(Rect rect, Scalar color, double alpha, Mat dest) {
+        Mat overlay = dest.clone();
+        Imgproc.rectangle(overlay, rect, color, -1);
+
+        Core.addWeighted(overlay, alpha, dest, 1, 1-alpha, dest);
+    }
+
+}
